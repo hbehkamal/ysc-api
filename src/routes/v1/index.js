49c0d3804cfc3ca4d6573998f1b6e6f11941684f
Hosein Behkamal <hbehkamal@gmail.com>
2021-06-28 21:33:05 +0430

    add base routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+const UserController = require(`${config.path.controller}/v1/userController`);
+const AuthController = require(`${config.path.controller}/v1/authController`);
+
+router.get("/", (req, res) => {
+  res.json("Welcome to YSC api homepage");
+});
+
+router.post("/register", AuthController.register.bind(AuthController));
+router.get("/users", UserController.index.bind(UserController));
 
 module.exports = router;
